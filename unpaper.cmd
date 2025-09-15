4acf094369a9f53763a2e4bf9106a725c147e4fd
filak <filak@users.noreply.github.com>
2025-09-15 14:57:26 +0200

    Updates

--- unpaper.cmd
+++ unpaper.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-python "%PDFWTF_HOME_DIR%\src\tools\unpaper.py" %*
+python "%PDFWTF_HOME_DIR%\src\tools\unpaper_wrap.py" %*
 REM When compiled replace with:
-rem "%PDFWTF_HOME_DIR%\dist\tools\unpaper.exe" %*
+rem "%PDFWTF_HOME_DIR%\dist\tools\unpaper_wrap.exe" %*
 endlocal
